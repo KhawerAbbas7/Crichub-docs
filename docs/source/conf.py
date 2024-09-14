79bdc8d79ad5692f7c069985de17b805bac26a8e
KhawerAbbas7 <85282172+KhawerAbbas7@users.noreply.github.com>
2024-09-14 23:06:10 +0500

    update docs/source/conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -36,3 +36,13 @@ html_theme_options = {
 }
 # -- Options for EPUB output
 epub_show_urls = 'footnote'
+## conf.py
+
+# These folders are copied to the documentation's HTML output
+html_static_path = ['_static']
+
+# These paths are either relative to html_static_path
+# or fully qualified paths (eg. https://...)
+html_css_files = [
+    'css/custom.css',
+]
